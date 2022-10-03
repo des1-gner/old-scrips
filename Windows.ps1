092c973389a6d66a921c8ae69ebb7a07ec346e78
Oisin Aeonn <99879353+des1-gner@users.noreply.github.com>
2022-10-03 21:57:08 +1100

    Add files via upload

---
+++ Windows.ps1
@@ -0,0 +1,18 @@
+﻿$var = Read-Host -Prompt "Enter File"
+
+$exist = Test-Path $var
+
+if ( $exist -eq $true)
+{ 
+	echo "File exists"    
+    $var2 = Read-Host -Prompt "Enter txt to search in $var"
+    Get-ChildItem -Path $var* -Filter *$var2 
+    Get-ChildItem -Path $var* -Filter *$var2 >> pattern.txt
+}
+
+else 
+{ 
+    echo "File not found. "
+}
+
+
